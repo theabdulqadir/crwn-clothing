6fa7dae0aa2c3efea7f61251940062578a942444
abduljilani <abdul.jilani@bold.com>
2020-01-13 22:38:36 +0530

    add basic setup to git

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './homepage.component';
+
 function App() {
   return (
-    <div className="App">
-      <div className>
+    <div>
+      <HomePage/>
     </div>
   );
 }
